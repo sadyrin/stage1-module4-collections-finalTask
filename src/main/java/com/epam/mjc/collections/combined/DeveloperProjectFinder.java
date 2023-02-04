46397685b153a26524999da2909cc74e7218d39e
sadyrin <sadyrin@gmail.com>
2023-02-04 15:08:26 +0200

    fix sort issue stage1-module4-collections-finalTask

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -18,7 +18,8 @@ public class DeveloperProjectFinder {
         Collections.sort(list, new Comparator<String>(){
             @Override
             public int compare(String s1, String s2) {
-                return s1.length() < s2.length() ? 1 : -1;
+                return s1.length() < s2.length() ? 1 :
+                    s1.length() == s2.length() ? s2.compareTo(s1) : -1;
             }
         });
 
